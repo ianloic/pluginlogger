0b2a7425cb2d63fc6cc2bf84c6d2d51d175ac74f
Ian McKellar <ian@mckellar.org>
2009-12-30 18:51:42 +0200

    give it a license

--- pluginlogger.cpp
+++ pluginlogger.cpp
@@ -1,4 +1,21 @@
-/* pluginlogger 0.1, a logging plugin wrapper */
+/* pluginlogger 0.1, a logging plugin wrapper 
+ * Copyright 2009 Ian McKellar <http://ian.mckellar.org/>
+ * */
+
+/*  This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 /* system headers for useful things */
 #include <dlfcn.h>
